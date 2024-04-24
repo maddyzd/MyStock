55b35586d90ad57121d04da8af83ebeea7a5c11f
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 18:54:22 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,14 +1,35 @@
-const MongoClient = require('mongodb').MongoClient;
+var http = require('http');
+var MongoClient = require('mongodb').MongoClient;
+var port = process.env.PORT || 3000;
 const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-	  
-console.log('hey')
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write("<h2>Hello World</h2>");
+  
   MongoClient.connect(connStr, function(err, db) {
-    
-  if(err) { console.log(err); }
-  else {
-    var dbo = db.db("Stock");
-	  var collection = dbo.collection('Companies');
-    console.log("Success!");
-	  db.close();
-  }
-});
+    if (err) { 
+      console.log(err);
+      res.write("Error connecting to database");
+      res.end();
+    } else {
+      var dbo = db.db("Stock");
+      var collection = dbo.collection('PublicCompanies');
+      collection.find({}).toArray(function(err, result) {
+        if (err) {
+          console.log(err);
+          res.write("Error fetching data from database");
+          res.end();
+        } else {
+          console.log("Success!");
+          res.write("<h3>Database Connection Successful!</h3>");
+          res.write("<pre>" + JSON.stringify(result, null, 2) + "</pre>");
+          res.end();
+        }
+        db.close();
+      });
+    }
+  });
+}).listen(port);
+
+console.log("Server running at http://localhost:" + port);

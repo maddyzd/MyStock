5900d4cc663b47f33d94aa678e19dbce80e1a75d
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 18:51:39 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const connStr= "mongodb+srv://mdumon:<password>@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 	  
 console.log('hey')
   MongoClient.connect(connStr, function(err, db) {

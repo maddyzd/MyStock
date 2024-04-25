509d62acca212c49d39e853e3eb0474678abdf0b
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 21:50:56 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,63 +1,69 @@
-var http = require('http');
-var url = require('url'); // Added 'url' module import
-var fs = require('fs'); // Added 'fs' module import
-var MongoClient = require('mongodb').MongoClient;
-var port = process.env.PORT || 3000;
-const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
+const MongoClient = require('mongodb').MongoClient;
+
+const port = process.env.PORT || 3000;
+const connStr = "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var urlObj = url.parse(req.url, true); // Added 'var' keyword before 'urlObj'
-    var path = urlObj.pathname;
+    const urlObj = url.parse(req.url, true);
+    const path = urlObj.pathname;
 
-    if (path == "/" ) {
-        file = "form.html";
-        fs.readFile(file, function(err, home) {
+    if (path === "/") {
+        fs.readFile('form.html', function(err, form) {
             if (err) {
                 console.error("Error reading file:", err);
                 res.writeHead(500, {'Content-Type': 'text/plain'});
                 res.end("Error loading HTML file.");
             } else {
-                res.write(home);
+                res.write(form);
                 res.end();
             }
         });
-    } else if (path == "/process" && req.method === "GET") {
-    var queryData = urlObj.query;
-    var searchTerm = queryData.searchTerm || '';
-    var searchType = queryData.searchType || 'name'; 
+    } else if (path === "/process" && req.method === "GET") {
+        const queryData = urlObj.query;
+        const searchTerm = queryData.searchTerm || '';
+        const searchType = queryData.searchType || 'name'; 
 
-    MongoClient.connect(connStr, function(err, db) {
-        if (err) { 
-            console.log(err);
-            res.write("Error connecting to database");
-            res.end();
-        } else {
-            var dbo = db.db("Stock");
-            var collection = dbo.collection('PublicCompanies');
-            var query = {};
-            if (searchType === "symbol") {
-                query = { "ticker": searchTerm };
+        MongoClient.connect(connStr, function(err, db) {
+            if (err) { 
+                console.log(err);
+                res.write("Error connecting to database");
+                res.end();
             } else {
-                query = { "company": searchTerm };
-            }
-            
-            collection.find(query).toArray(function(err, result) {
-                if (err) {
-                    console.log(err);
-                    res.write("Error fetching data from database");
-                    res.end();
+                const dbo = db.db("Stock");
+                const collection = dbo.collection('PublicCompanies');
+                let query = {};
+                if (searchType === "symbol") {
+                    query = { "ticker": searchTerm };
                 } else {
-                    console.log("Success!");
-                    console.log(result); // Print the result to console
-                    res.write("<h3>Search Results</h3>");
-                    result.forEach(function(doc) {
-                        res.write(`<p>Company: ${doc.company}, Symbol: ${doc.ticker}, Price: ${doc.price}</p>`);
-                    });
-                    res.end();
+                    query = { "company": searchTerm };
                 }
-                db.close();
-            });
-        }
-    });
-}
+                
+                collection.find(query).toArray(function(err, result) {
+                    if (err) {
+                        console.log(err);
+                        res.write("Error fetching data from database");
+                        res.end();
+                    } else {
+                        console.log("Success!");
+                        console.log(result); // Print the result to console
+                        res.write("<h3>Search Results</h3>");
+                        result.forEach(function(doc) {
+                            res.write(`<p>Company: ${doc.company}, Symbol: ${doc.ticker}, Price: ${doc.price}</p>`);
+                        });
+                        res.end();
+                    }
+                    db.close();
+                });
+            }
+        });
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Not Found");
+    }
+}).listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});

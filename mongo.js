cdd32c838559e9447f0c0d1f210bd04969381f20
maddyzd <38566228+maddyzd@users.noreply.github.com>
2024-04-24 21:08:51 -0400

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -1,41 +1,55 @@
 var http = require('http');
+var url = require('url'); // Added 'url' module import
+var fs = require('fs'); // Added 'fs' module import
 var MongoClient = require('mongodb').MongoClient;
 var port = process.env.PORT || 3000;
 const connStr= "mongodb+srv://mdumon:mydb123@cluster0.rvujnyd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-    urlObj = url.parse(req.url,true)
-  path = urlObj.pathname;
-  if (path == "/")
-  {
-    file="form.html";
-    fs.readFile(file, function(err, home) {
-    res.write(home);
-    res.end();
-    }) else if (path == "/process")
-  
-  MongoClient.connect(connStr, function(err, db) {
-    if (err) { 
-      console.log(err);
-      res.write("Error connecting to database");
-      res.end();
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var urlObj = url.parse(req.url, true); // Added 'var' keyword before 'urlObj'
+    var path = urlObj.pathname;
+
+    if (path == "/") {
+        file = "form.html";
+        fs.readFile(file, function(err, home) {
+            if (err) {
+                console.error("Error reading file:", err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end("Error loading HTML file.");
+            } else {
+                res.write(home);
+                res.end();
+            }
+        });
+    } else if (path == "/process") { // Corrected the else if syntax
+        MongoClient.connect(connStr, function(err, db) {
+            if (err) { 
+                console.log(err);
+                res.write("Error connecting to database");
+                res.end();
+            } else {
+                var dbo = db.db("Stock");
+                var collection = dbo.collection('PublicCompanies');
+                collection.find({}).toArray(function(err, result) {
+                    if (err) {
+                        console.log(err);
+                        res.write("Error fetching data from database");
+                        res.end();
+                    } else {
+                        console.log("Success!");
+                        res.write("<h3>Database Connection Successful!</h3>");
+                        res.write("<pre>" + JSON.stringify(result, null, 2) + "</pre>");
+                        res.end();
+                    }
+                    db.close();
+                });
+            }
+        });
     } else {
-      var dbo = db.db("Stock");
-      var collection = dbo.collection('PublicCompanies');
-      collection.find({}).toArray(function(err, result) {
-        if (err) {
-          console.log(err);
-          res.write("Error fetching data from database");
-          res.end();
-        } else {
-          console.log("Success!");
-          res.write("<h3>Database Connection Successful!</h3>");
-          res.write("<pre>" + JSON.stringify(result, null, 2) + "</pre>");
-          res.end();
-        }
-        db.close();
-      });
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end("Not Found");
     }
-  });
-}).listen(port);
+}).listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
